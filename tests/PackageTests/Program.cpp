3e13973756eb74c437815bc2d7503867ac274cd6
Bas Stroosnijder <bas@strootje.com>
2017-11-04 19:07:48 +0100

    Updated packageTest to be more meaningful

--- tests/PackageTests/Program.cpp
+++ tests/PackageTests/Program.cpp
@@ -1,7 +1,25 @@
-#include <Grawlog/Logger.hpp>
+#include <Pulsr.Core/Window/Manager.hpp>
+
+class TestWindowManager : public Pulsr::Core::Window::Manager
+{
+public:
+	void Create( const std::string& name )
+	{
+	};
+
+	bool HasWork()
+	{
+		return false;
+	};
+
+	void Update()
+	{
+	};
+};
 
 int main(int argc, char* argv[])
 {
-	LOG(INFO) << "Hello, Grawlog";
+	TestWindowManager windowManager;
+	windowManager.Create("Hello, World");
 	return 0;
 }
